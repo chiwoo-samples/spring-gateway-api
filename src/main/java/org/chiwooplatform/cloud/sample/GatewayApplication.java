71f9efb4d2471a7a2a49808eea55aa667ea5a8ac
seonbo.shim <seonbo.shim@SeonBoShim.hwk.net>
2017-03-23 17:48:44 +0900

    shutdown Endpoint 추가

--- src/main/java/org/chiwooplatform/cloud/sample/GatewayApplication.java
+++ src/main/java/org/chiwooplatform/cloud/sample/GatewayApplication.java
@@ -2,8 +2,10 @@ package org.chiwooplatform.cloud.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@RefreshScope
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication
